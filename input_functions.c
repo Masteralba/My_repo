675e0b0e4a43a8ed59096a7b978b3bb2cfbe210a
GlebKhorchev <super.koio777@yandex.ru>
2024-05-16 13:26:55 +0300

    all haha

--- input_functions.c
+++ input_functions.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "help_functions.c"
-#include "struct.c"
+#include "help_functions.h"
+#include "struct.h"
 
 #define BITS_PER_PIXEl 24
 #define PLANES 1
@@ -23,13 +23,29 @@ Rgb **read_bmp(char file_name[], BitmapFileHeader* bmfh, BitmapInfoHeader* bmif)
         error("Error in 'read_bmp': file is not bmp format", bmfh, bmif, NULL);
     }
     readed += fread(bmif, 1, sizeof(BitmapInfoHeader), f);
+    if ( bmif->headerSize != 40)
+    {
+        fclose(f);
+        error("Error in 'read_bmp': cant handle this version of bmp", bmfh, bmif, NULL);
+    }
     if ( bmif->planes != PLANES)
     {
+        fclose(f);
         error("Error in 'read_bmp': planes are not equal to 1", bmfh, bmif, NULL);
     }
     if ( bmif->bitsPerPixel != BITS_PER_PIXEl)
     {
         error("Error in 'read_bmp': bits_per_pixel are not equal to 24", bmfh, bmif, NULL);
+    }
+    if ( bmif->compression != 0)
+    {
+        fclose(f);
+        error("Error in 'read_bmp': cant handle images with compression", bmfh, bmif, NULL);
+    }
+    if ( bmif->colorsInColorTable != 0)
+    {
+        fclose(f);
+        error("Error in 'read_bmp': cant handle images with color table", bmfh, bmif, NULL);
     }
     unsigned int H = bmif->height;
     unsigned int W = bmif->width;
@@ -37,9 +53,19 @@ Rgb **read_bmp(char file_name[], BitmapFileHeader* bmfh, BitmapInfoHeader* bmif)
     if ( padding ) padding = 4 - padding;
     size_t total_size = W * sizeof(Rgb) + padding;
     Rgb **arr = (Rgb**)malloc(H * sizeof(Rgb*));
+    if ( arr == NULL)
+    {
+        fclose(f);
+        error("Error in 'read_bmp': cannot allocate memory", bmfh, bmif, arr);
+    }
     for(int i = 0; i < H; i++)
     {
-        arr[i] = (Rgb*)calloc(1, total_size);   
+        arr[i] = (Rgb*)calloc(1, total_size);
+        if ( arr[i] == NULL)
+        {
+            fclose(f);
+            error("Error in 'read_bmp': cannot allocate memory", bmfh, bmif, arr);
+        }
         readed += fread(arr[i], 1, W * sizeof(Rgb) + padding,f);
     }
     if (readed!= bmfh->filesize)
@@ -48,30 +74,6 @@ Rgb **read_bmp(char file_name[], BitmapFileHeader* bmfh, BitmapInfoHeader* bmif)
     }
     fclose(f);
     return arr;
-}
-
-// malloc(W * sizeof(Rgb) + padding)
-
-void print_file_header(BitmapFileHeader header)
-{
-    printf("signature:\t%x (%hu)\n", header.signature,header.signature);
-    printf("filesize:\t%x (%u)\n", header.filesize,header.filesize);
-    printf("reserved1:\t%x (%hu)\n", header.reserved1,header.reserved1);
-    printf("reserved2:\t%x (%hu)\n", header.reserved2,header.reserved2);
-    printf("pixelArrOffset:\t%x (%u)\n", header.pixelArrOffset, header.pixelArrOffset);
-}
-void print_info_header(BitmapInfoHeader header){
-    printf("headerSize:\t%x (%u)\n", header.headerSize,header.headerSize);
-    printf("width: \t%x (%u)\n", header.width, header.width); 
-    printf("height: \t%x (%u)\n", header.height,header.height);
-    printf("planes: \t%x (%hu)\n", header.planes,header.planes);
-    printf("bitsPerPixel:\t%x (%hu)\n", header.bitsPerPixel, header.bitsPerPixel);
-    printf("compression:\t%x (%u)\n", header.compression, header.compression);
-    printf("imageSize:\t%x (%u)\n", header.imageSize,header.imageSize);
-    printf("xPixelsPerMeter:\t%x (%u)\n", header.xPixelsPerMeter, header.xPixelsPerMeter);
-    printf("yPixelsPerMeter:\t%x (%u)\n", header.yPixelsPerMeter, header.yPixelsPerMeter);
-    printf("colorsInColorTable:\t%x (%u)\n", header.colorsInColorTable, header.colorsInColorTable);
-    printf("importantColorCount:\t%x (%u)\n", header.importantColorCount, header.importantColorCount);
 }
 
 
