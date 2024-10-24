3c8d928e274cd152c0b8dc64b06366c69acba213
gleb <super.koio777@yandex.ru>
2024-10-24 16:31:52 +0300

    clean

--- function_2.h
+++
@@ -1,3 +0,0 @@
-#include "struct.h"
-
-Rgb** function_2(Rgb** arr, BitmapFileHeader* bmfh, BitmapInfoHeader* bmif, const char* change_colour, unsigned int value);
--- function_3.h
+++
@@ -1,3 +0,0 @@
-#include "struct.h"
-
-Rgb** function_3(Rgb** arr, BitmapFileHeader* bmfh, BitmapInfoHeader* bmif, unsigned int n, unsigned int m, unsigned int thickness, Rgb color);
