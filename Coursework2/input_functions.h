a39fd40a5010e502c76f78273f77fefcacebaa14
gleb <super.koio777@yandex.ru>
2025-08-12 10:19:30 +0300

    delete not cpp

--- Coursework2/input_functions.h
+++
@@ -1,5 +0,0 @@
-#include "struct.h"
-
-Rgb **read_bmp(char file_name[], BitmapFileHeader* bmfh, BitmapInfoHeader* bmif);
-
-void write_bmp(char file_name[], Rgb **arr, unsigned int H, unsigned int W, BitmapFileHeader bmfh, BitmapInfoHeader bmif);
